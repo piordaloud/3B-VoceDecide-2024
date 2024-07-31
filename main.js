7497d0f8a64bca26e866951d719dd93323185df6
gian SaydeN <106679444+piordaloud@users.noreply.github.com>
2024-07-31 08:49:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,50 +2,88 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
-const textoResultado = document.querySelector('.texto-resultado');
+const TextoResultado = document.querySelector('.texto-resultado');
+
 const perguntas = [
     {
-        enunciado: "Pergunta 01",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
         ]
+
     },
     {
-        enunciado: "Pergunta 02",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
         ]
+
     },
     {
-        enunciado: "Pergunta 03",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
         ]
+
     },
     {
-        enunciado: "Pergunta 04",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
         ]
+
     },
     {
-        enunciado: "Pergunta 05",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
         ]
-    },
+
+    }
 ]
 
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textcontent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
+
+function mostraPergunta() {
+    if (atual >= perguntaAtual.length) {
+        mostreResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoalternativas = document.createElement("button");
+        botaoalternativas.textContent = alternativa.texto;
+        botaoalternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+    }
+}
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    atual++;
+    mostraPergunta();
+}
+
+function mostreResultado() {
+
+    caixaPerguntas.textContent = " Em 2049..... ";
+    TextoResultado.textcontent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
